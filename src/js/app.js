a603b1dd69780bc21a5a5239b96784a473abf47c
tifred <tifred@lmi.net>
2015-11-23 09:26:05 -0800

    moved to src and dist production environment

---
+++ src/js/app.js
@@ -0,0 +1,148 @@
+// The initial locations.
+// Using an object for each array element
+// allows room to easily add more criteria.
+// e.g. "restaurant", "street", "address", "phone"
+
+var initialLocations = [
+  {
+     name: "Brooklyn",
+     state: "NY",
+     visible: true
+  },
+  {
+     name: "Queens",
+     state: "NY",
+     visible: true
+  },
+  {
+     name: "Harlem",
+     state: "NY",
+     visible: true
+  },
+  {
+     name: "Hell's Kitchen",
+     state: "NY",
+     visible: true
+  },
+  {
+     name: "Washington Heights",
+     state: "NY",
+     visible: true
+  },
+  {
+     name: "Soho",
+     state: "NY",
+     visible: true
+  }
+];
+
+// Location: Constructor that will be used for each location.
+// All properties are observables.
+
+var Location = function(data) {
+  this.name = ko.observable(data.name);
+  this.state = ko.observable(data.state);
+  this.visible = ko.observable(data.visible);
+};
+
+var ViewModel = function() {
+  var self = this;
+
+  // locList is the main observable Array.
+  // It will hold all the objects built with the 
+  // "Location" constructor.
+
+  this.locList = ko.observableArray([]);
+  this.prompt = ko.observable("Type Location Here");
+  this.toggleListView = ko.observable(true);
+
+  // buildList: Build the locList from the initialLocations array.
+  // This is the initial list displayed, 
+  // in which all locations are visible.
+
+  this.buildList = function() {
+    initialLocations.forEach(function(locItem) {
+      self.locList.push(new Location(locItem));
+    });
+  };
+  this.buildList();
+
+  // buildMap: Build the map.
+  // The mapLocations array contains all visible locations.
+  // Calls initializeMap, which is defined in the google.Map.js file.
+  // "false" parameter means to NOT bounce the marker for each location.
+  // "true" parameter means DO bounce the marker for each location. 
+  // Only run after whole window is loaded, or errors may occur.
+
+  this.buildMap = function() {
+    var mapLocations = [];
+    self.locList().forEach(function(loc) {
+      if (loc.visible()) {
+        mapLocations.push('' + loc.name() + ', ' + loc.state() + '');
+      }
+    });
+    window.addEventListener('load', initializeMap(mapLocations, false));
+  };
+  this.buildMap();
+
+  // filterList: Filter list and map of locations based on user input.
+  // formElement is the form element, passed in by Knockout.
+  // inputString is the value the user typed in.
+  // regexp is the same string, but formatted as a regular expression.
+  // the test method will return true if a match is found given the "place".
+  // based on that test,  the visible status of each location is set to true or false.
+  // the list view will update automatically because of a foreach data-bind to the ul tag.
+  // finally, the map is rebuilt.
+
+  this.filterList = function(formElement) {
+    var inputString = $(formElement).children("input").val();
+    var regexp = new RegExp(inputString);
+    self.locList().forEach(function(loc) {
+      var place =  loc.name() + loc.state();
+      regexp.test(place) ? loc.visible(true) : loc.visible(false);
+    });
+    this.buildMap();
+  };
+
+  // Reset: Reset button to clear the filter form.
+  // Sets all locations to visible.
+  // Returns "prompt" to original value.
+
+  this.reset = function() {
+    // to clear the browser cache of input form:
+    document.getElementById("filter-form").reset();  
+    self.prompt("Type Location Here");
+    self.locList().forEach(function(loc) {
+      loc.visible(true);
+    });
+    this.buildMap();
+  };
+
+  // Edit: Clear out the prompt text when a user clicks on the form.
+
+  this.edit = function() {
+    this.prompt("");
+  };
+
+  // toggleListPanel: Clicking on menu icon (which is only visible below 768px in width)
+  // Will either hide or show the entire list panel.
+  // toggleListView is data bound to the div of the entire
+  // list view panel.
+  // I think this could be done more simply.
+
+  this.toggleListPanel = function() {
+    this.toggleListView(!this.toggleListView());
+  };
+
+  // bounceMarker: Clicking on list item makes associated marker in map bounce.
+  // Only applies to the single, clicked-upon item.
+  // The "true" parameter means initializeMap will do the right thing
+  // for just this one location.  All other locations are left alone.
+  
+  this.bounceMarker = function(loc) {
+    var bounceLocation = [];
+    bounceLocation.push('' + loc.name() + ', ' + loc.state() + '');
+    window.addEventListener('load', initializeMap(bounceLocation, true));
+  };
+};
+ko.applyBindings(new ViewModel());
---
+++ src/js/googleMap.js
@@ -0,0 +1,165 @@
+/*
+The rough order in which things happen:
+
+  "map" variable is set up to attach final map to an HTML element.
+  "initializeMap calls "pinPoster".
+  "pinPoster" loops through locations array, gets "results" object from Google.
+  "callback" is run on each set of results.
+  "callback" does ajax query with New York Times API for extra info on each location.
+  "callback" calls "createMapMarker".
+  "createMapMarker" makes the marker, makes the info window.
+  "createMapMarker" sets up event listener to bounce marker on click or to bounce now.
+*/
+
+
+var map;                    // declares a global map variable
+var currentMarker = null;   // used to allow only one bouncing marker at once.
+
+// initializeMap: the main function.
+// Run from ViewModel in the "app.js" file.
+// "locations" is an array with elements like: "Brooklyn, NY"
+// "bounce" is a boolean.
+//    true means to set one single location's marker bouncing right now.
+//    false means to set an event listener to make that happen later.
+
+// Note: if bounce was set to true when initializeMap was called,
+// then the "locations" array was also set to only have a single location in it.
+
+function initializeMap(locations, bounce) {
+
+  // Empty now, but may be useful in future.
+  var mapOptions = {};
+
+  // build instance of map from Google's constructor.
+  // the final map image will be part of the "#map" element in the HTML.
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+  // Build map marker and info window per location.
+
+  function createMapMarker(placeData, nytInfo) {
+
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: nytInfo
+    });
+
+    // oneBounceOnly: a helper function.
+    // when marker is clicked, stop bouncing on any other bouncing markers
+    // if they exist.
+    // uses the currentMarker variable, which must be global.
+
+    function oneBounceOnly(marker) {
+        if (currentMarker) currentMarker.setAnimation(null);
+        currentMarker = marker;
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    };
+
+    // Here is where the boolean parameter "bounce" matters.
+    // If bounce is true, open the info window and bounce the marker NOW.
+    // There will only be one location in the locations array.
+
+    // Otherwise, set up an event listener to open the info window
+    // and bounce the marker when there is a click LATER.
+
+    if (bounce) {
+        infoWindow.open(map, marker);
+        oneBounceOnly(marker);
+    } else {
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(map, marker);
+        oneBounceOnly(marker);
+      });
+    }
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  // callback: run by service.textSearch when it gets results back.
+  // Use "status" to be sure results were received.
+
+  // If results don't come back, return error message to #map element.
+  // You can test this by mangling "google.maps.places.PlacesServiceStatus.OK" below.
+  // insert "XYZ" anywhere in that string and the lookup will fail.
+
+  // If results do come back, call New York Times data API.
+  // Build string "nytinfo" of "li"s with first three most recent articles about location.
+  // Uses an AJAX call with a done and fail method.
+  // It's important to acquire this info before running createMapMarker.
+  // That function must have all the info from the NYT, or it will have no info to display.
+
+  // Finally, call createMapMarker with results array and nytInfo string.
+
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var name = results[0].formatted_address.split(",")[0];
+      var nytQuery = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + name + '&sort=newest&api-key=8d6910d8cc45e32cb31931220a4d3c4b:5:73416722';
+      $.getJSON(nytQuery)
+         .done(function(data) {
+           var items = [];
+           items.push("<ul>");
+           for (var i = 0; i < 3; i++ ) {
+             items.push( "<li class='article'> <a href='" + data.response.docs[i].web_url + "'> " + data.response.docs[i].headline.main + "</a> <p>" + data.response.docs[i].snippet + "</p> </li>");
+           }
+           items.push("</ul>");
+           var nytInfo = items.join("");
+           createMapMarker(results[0], nytInfo);
+         })
+         .fail(function() {
+           var failText = "<p>Failed to Load the NYT articles.  Sorry.</p>";
+           var nytInfo = failText;
+           createMapMarker(results[0], nytInfo);
+         });
+    } else {
+      $('#map').append('<h3>No results from Google Maps API.  Consider yourself lost.</h3>');
+    }
+  }
+
+  function pinPoster(locations) {
+
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterate through the array of locations.
+    // Create a search object for each location.
+    // The callback will then go on to call createMapMarker.
+
+    for (var i = 0; i < locations.length; i++) {
+      // The search request object:
+      var request = {
+        query: locations[i]
+      };
+      // Go get results for the given query.
+      // When the results arrive, run the callback function.
+      service.textSearch(request, callback);
+    }
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // pinPoster(locations) creates pins on the map for each location.
+  // This is the real start of the function.
+  pinPoster(locations);
+}
+
+// Vanilla JS way to listen for resizing of the window and adjust map bounds
+window.addEventListener('resize', function(e) {
+  map.fitBounds(mapBounds);
+});
